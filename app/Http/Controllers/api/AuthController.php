6aba5d9962226a3b4abe6ca0d117abadc14725cf
8ballali <iqbalalisaidil@outlook.com>
2022-06-07 19:43:28 +0700

    Add some module and fix Models

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -80,5 +80,15 @@ class AuthController extends Controller
             ]);
         }
     }
+    public function logout () {
+        $user = request()->user(); //or Auth::user()
+        // Revoke current user token
+        $user->tokens()->where('id', $user->currentAccessToken()->id)->delete();
+        return response()->json([
+            'success' => true,
+            'message' => 'Logout Berhasil',
+
+        ],200);
+    }
 
 }
